436be95960b10f3ab29f8f0ac51066bc11bc675d
Caroline <Cluce059@gmail.com>
2021-09-12 20:17:13 -0400

    home page up

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
-
-const routes = require('./routes');
-
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const exhbs = require('express-handlebars');
+const hbs = exhbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +13,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+
+app.set('view engine', 'handlebars');
 
 //sync method to establish db connection...if it does not find a table it will create one for you
 //force: true same as drop table if exists..can b annoying
