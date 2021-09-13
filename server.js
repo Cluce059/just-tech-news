ae3ace30c88411bc82c2ec2b3fae08d00a7eec84
Caroline <Cluce059@gmail.com>
2021-09-13 15:13:18 -0400

    login functionality

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
-const exhbs = require('express-handlebars');
-const hbs = exhbs.create({});
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.use(express.urlencoded({ extended: true }));
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-app.use(routes);
+app.use(session(sess));
 
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
-
 app.set('view engine', 'handlebars');
 
-//sync method to establish db connection...if it does not find a table it will create one for you
-//force: true same as drop table if exists..can b annoying
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () =>  console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
