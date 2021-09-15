a2eb53e0b8216add0d03d207e1d33bca2a571fb9
Caroline <Cluce059@gmail.com>
2021-09-15 11:42:55 -0400

    end of 14.3.4{

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
 
@@ -81,13 +82,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+//make sure session exists before doing anything
+if(req.session){
+  //pass session id along with all destructured properties from req.body
+  Post.upvote({ ...req.body, user_id: req.session.user_id}, {Vote, Comment,User })
+  .then(updatedVoteData => res.json(updatedVoteData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+}
 });
 
 router.put('/:id', (req, res) => {
